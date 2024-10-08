2f4a530cd14448a2a792b13c92ad39fa072a4fee
anasnaeem80 <anas.naeem.998@gmail.com>
2024-10-09 02:20:52 +0500

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   base: "./",
   build: {
     rollupOptions: {
-      // External dependencies here only if they are loaded from external sources like CDNs
-      // external: ['react', 'react-dom'],
+      // external: ['react', 'react-dom'], // Remove external temporarily for debugging
     },
   },
 });
